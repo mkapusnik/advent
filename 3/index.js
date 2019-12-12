819bd467b2bbbe43e1cb7d672e1f0ac9d5d490fa
Kapusnik, Michal <mkapusnik@netsuite.com>
2019-12-12 14:26:17 +0100

    Day 3

--- 3/index.js
+++ 3/index.js
@@ -1,21 +1,9 @@
-const lineReader = require('line-reader');
+var fs = require("fs");
 
-var positions = [];
+var content = fs.readFileSync('input.txt', 'utf8');
+var lines = content.split(/\r?\n/);
 
-function visitPosition(x, y, line) {
-    var index = "X" + x + "Y" + y;
-    var existing = positions[index] || [];
-    if(!existing.includes(line)) {
-        existing.push(line);
-        positions[index] = existing;        
-    } else {
-        console.log("Visitor: ", index, ": ", positions[index]);
-    }
-}
-var lineId = 0;
-
-function execute(line) {
-    lineId++;
+function getPositions(positions, line, lineId) {
     var instructions = line.split(",");
     var x = 0;
     var y = 0;
@@ -37,26 +25,30 @@ function execute(line) {
                     x++;
                     break;
             }
-            visitPosition(x, y, lineId);
+            var index = "X" + x + "Y" + y;
+            var existing = positions[index] || { lines: [], x: x, y: y };
+            if(!existing.lines.includes(lineId)) {
+                existing.lines.push(lineId);
+                positions[index] = existing;        
+            }
         }
     });
-};
+}
 
-lineReader.eachLine('input.txt', function(line, last) {
-    execute(line);
-
-    if(last) {
-        var crossings = positions.filter(function (visitors) {
-            return visitors.length > 1;
-        });
-
-        console.log("Positions: ");
-        positions.forEach(function(value, index) {
-            console.log(index);
-        });
-        console.log("Crossing positions: ");
-        crossings.forEach(function(value, index) {
-            console.log(index);
-        });
-    }
+var positions = [];
+lines.forEach(function(line, index) {
+    getPositions(positions, line, index);
 });
+
+var distance;
+for (var key in positions) {
+    var details = positions[key];
+    if(details.lines.length > 1) {
+        var currentDistance = Math.abs(details.x) + Math.abs(details.y);        
+        if(!distance || currentDistance < distance) {
+            distance = currentDistance;
+        }
+        console.log("Current: ", currentDistance);
+    }
+}
+console.log("Distance: ", distance);
