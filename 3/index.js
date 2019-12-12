f78cf08d8cfb3ee57568904369e01c5a1744a6f3
Kapusnik, Michal <mkapusnik@netsuite.com>
2019-12-12 14:31:54 +0100

    Day 3, bonus

--- 3/index.js
+++ 3/index.js
@@ -7,10 +7,12 @@ function getPositions(positions, line, lineId) {
     var instructions = line.split(",");
     var x = 0;
     var y = 0;
+    var stepCount = 0;
     instructions.forEach(function(instruction) {
         var steps = Number(instruction.substring(1));
         var direction = instruction[0];
         for(var i = 0; i < steps; i++) {
+            stepCount++;
             switch(direction) {
                 case "U":
                     y++                
@@ -26,9 +28,10 @@ function getPositions(positions, line, lineId) {
                     break;
             }
             var index = "X" + x + "Y" + y;
-            var existing = positions[index] || { lines: [], x: x, y: y };
+            var existing = positions[index] || { lines: [], x: x, y: y, steps: 0 };
             if(!existing.lines.includes(lineId)) {
                 existing.lines.push(lineId);
+                existing.steps += stepCount;
                 positions[index] = existing;        
             }
         }
@@ -41,14 +44,22 @@ lines.forEach(function(line, index) {
 });
 
 var distance;
+var shortest;
 for (var key in positions) {
     var details = positions[key];
     if(details.lines.length > 1) {
         var currentDistance = Math.abs(details.x) + Math.abs(details.y);        
+
         if(!distance || currentDistance < distance) {
             distance = currentDistance;
         }
-        console.log("Current: ", currentDistance);
+        if(!shortest || details.steps < shortest) {
+            shortest = details.steps;
+        }
+
+        //console.log("Current: ", currentDistance);
+        //console.log("Current Shortest: ", details.steps);
     }
 }
 console.log("Distance: ", distance);
+console.log("Shortest: ", shortest);
