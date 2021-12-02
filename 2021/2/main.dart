0ee7b80c99ee3b4e31f59a90231e6c58ab6b3155
Kapusnik, Michal <mkapusnik@netsuite.com>
2021-12-02 16:03:30 +0100

    Day 2: second star

--- 2021/2/main.dart
+++ 2021/2/main.dart
@@ -4,14 +4,14 @@ Future<void> main() async {
   final File input = File('input.txt');
   final List<String> lines = await input.readAsLines();
 
-  int position = 0, depth = 0;
+  int position = 0, depth = 0, aim = 0;
   for (final line in lines) {
     final List<String> instruction = line.split(' ');
-    final int speed = int.tryParse(instruction[1]) ?? 0;
+    final int arg = int.tryParse(instruction[1]) ?? 0;
     switch(instruction[0]) {
-      case 'forward': position += speed; break;
-      case 'up': depth -= speed; break;
-      case 'down': depth += speed; break;
+      case 'forward': position += arg; depth += (aim * arg); break;
+      case 'up': aim -= arg; break;
+      case 'down': aim += arg; break;
       default: throw "Invalid instruction: ${instruction[0]}";
     }
   }
