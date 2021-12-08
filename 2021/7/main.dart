eb1bd4b4de40bd9a35321b8e243d53c792089212
Kapusnik, Michal <mkapusnik@netsuite.com>
2021-12-08 18:39:42 +0100

    Day 7: first

--- 2021/7/main.dart
+++ 2021/7/main.dart
@@ -1,27 +1,32 @@
 import 'dart:io';
 
 Future<void> main() async {
-  final File input = File('6/input.txt');
+  final File input = File('7/input.txt');
   final List<String> lines = await input.readAsLines();
   //final List<String> lines = [ '3,4,3,1,2' ];
   final List<int> initial = lines[0].split(',').map((e) => int.parse(e)).toList();
+  final List<int> results = [];
 
-  final List<int> timers = List<int>.generate(9, (_) => 0);
+  int min = initial.reduce((value, element) => (value >= element) ? element : value);
+  int max = initial.reduce((value, element) => (value >= element) ? value : element);
+  print("Range: $min - $max");
 
-  initial.forEach((timer) {
-    timers[timer] += 1;
-  });
-
-  for(int i = 0; i < 256; i++) {
-    print("Tick: $i");
-    int newborns = timers[0];
-    for(int t = 1; t < timers.length; t++) {
-      timers[t-1] = timers[t];
+  int lowest = 99999999999999999;
+  int target = min;
+  for(int i = min; i < max; i++) {
+    int consumption = calculateFuel(i, initial);
+    if(consumption < lowest) {
+      lowest = consumption;
+      target = i;
     }
-    timers[6] += newborns;
-    timers[8] = newborns;
+    results.insert(i, consumption);
   }
 
-  print("Total size: ${timers.reduce((value, element) => value+element)}");
-  print("Results: $timers");
+
+  print("Results: $results");
+  print("Lowest fuel: $lowest | Target: $target");
+}
+
+int calculateFuel(int target, List<int> crabs) {
+  return crabs.map((e) => (target - e).abs()).reduce((value, element) => value + element);
 }
