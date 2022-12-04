5b23d589bd543fa232eedee161f8dc75e4a63798
Kapusnik, Michal <michal.kapusnik@oracle.com>
2022-12-04 22:42:53 +0100

    Day 2: second star

--- 2022/2/main.dart
+++ 2022/2/main.dart
@@ -6,16 +6,16 @@ final Map<String, String> elfSigns = {
   'C' : 'Scissors'
 };
 
-final Map<String, String> mySigns = {
-  'X' : 'Rock',
-  'Y' : 'Paper',
-  'Z' : 'Scissors'
-};
-
 final Map<String, int> shapeScore = {
   'Rock' : 1,
   'Paper' : 2,
   'Scissors' : 3
+};
+
+final Map<String, int> roundPoints = {
+  'X' : 0,
+  'Y' : 3,
+  'Z' : 6
 };
 
 Future<void> main() async {
@@ -26,21 +26,35 @@ Future<void> main() async {
   for (final line in lines) {
     List<String> game = line.split(' ');
     final String elf = elfSigns[game[0]]!;
-    final String me = mySigns[game[1]]!;
-    final int roundPoints = gamePoints(elf, me);
-    final int shapePoints = shapeScore[me]!;
-    print('Round: $elf vs $me: $roundPoints + $shapePoints');
-    score += roundPoints + shapePoints;
+    final int points = roundPoints[game[1]]!;
+
+    final int shapePoints = myShapePoints(elf, points);
+
+    //print('Round: $elf vs $me: $roundPoints + $shapePoints');
+    score += points + shapePoints;
     print('Semi-total: $score');
   }
 
   print("Total score: $score");
 }
 
-int gamePoints(String elf, String me) {
-  if(me == elf) return 3;
-  if(me == 'Rock' && elf == 'Scissors') return 6;
-  if(me == 'Scissors' && elf == 'Paper') return 6;
-  if(me == 'Paper' && elf == 'Rock') return 6;
-  return 0;
+int myShapePoints(String elf, int roundPoints) {
+  if(roundPoints == 3) {
+    return shapeScore[elf]!;
+  }
+  if(roundPoints == 6) {
+    switch(elf) {
+      case 'Rock': return shapeScore['Paper']!;
+      case 'Paper': return shapeScore['Scissors']!;
+      case 'Scissors': return shapeScore['Rock']!;
+    }
+  }
+  if(roundPoints == 0) {
+    switch(elf) {
+      case 'Rock': return shapeScore['Scissors']!;
+      case 'Paper': return shapeScore['Rock']!;
+      case 'Scissors': return shapeScore['Paper']!;
+    }
+  }
+  throw 'Invalid game state';
 }
