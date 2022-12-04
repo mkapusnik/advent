10d324e6f4e961e26cad9a86008650498fccb571
Kapusnik, Michal <michal.kapusnik@oracle.com>
2022-12-04 18:00:00 +0100

    Day 1: preparations

---
+++ 2022/1/main.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final File input = File('1/input.txt');
+  final List<String> lines = await input.readAsLines();
+
+  int increases = 0;
+  int? measurementA, measurementB, measurementC;
+  for (final line in lines) {
+    int current = int.tryParse(line) ?? 0;
+    if(measurementA == null) {
+      measurementA = current;
+      continue;
+    }
+    if(measurementB == null) {
+      measurementB = current;
+      continue;
+    }
+    if(measurementC == null) {
+      measurementC = current;
+      continue;
+    }
+    int prevSum = measurementA + measurementB + measurementC;
+    int currentSum = measurementB + measurementC + current;
+
+    if(prevSum < currentSum) {
+      increases++;
+    }
+    measurementA = measurementB;
+    measurementB = measurementC;
+    measurementC = current;
+  }
+  print("Num of increases: $increases");
+}
