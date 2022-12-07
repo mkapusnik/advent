223d85868473f56c7242ca3099e028cc277cd760
Kapusnik, Michal <michal.kapusnik@oracle.com>
2022-12-08 00:25:23 +0100

    Day 4: first star

---
+++ 2022/4/main.dart
@@ -0,0 +1,30 @@
+import 'dart:core';
+import 'dart:io';
+
+Future<void> main() async {
+  final File input = File('4/input.txt');
+  final List<String> lines = await input.readAsLines();
+
+  int overlaps = 0;
+  for (String line in lines) {
+    final List<String> elves = line.split(',');
+    final List<int> first = assignedSections(elves[0]);
+    final List<int> second = assignedSections(elves[1]);
+
+    if(first.every((element) => second.contains(element)))
+      overlaps++;
+    else if(second.every((element) => first.contains(element)))
+      overlaps++;
+  }
+
+  print("-----------------------------");
+  print("Total overlaps: $overlaps");
+}
+
+List<int> assignedSections(String input) {
+  final List<String> range = input.split('-');
+  int start = int.parse(range[0]);
+  int end = int.parse(range[1]);
+
+  return List.generate(end - start + 1, (index) => start + index);
+}
